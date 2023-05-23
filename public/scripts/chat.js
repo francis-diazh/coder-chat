02d625d0836c9b78faf51e7ee373842e6615a323
Francis Diaz <diazh.francis@gmail.com>
2023-05-22 21:10:46 -0300

    server listo para chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,7 +1,33 @@
-let user;
+let socket = io();
+let userName;
 
 Swal.fire({
-    title: "Ingrese su nombre",
-    input: "text"
-  
-  })
+  title: "Ingrese su nombre",
+  input: "text",
+  inputValidator: (value) => !value && "please write your name",
+  allowOutsideClick: false,
+}).then((res) => {
+  userName = res.value;
+  document.getElementById("userName").innerHTML = userName;
+  socket.emit("auth", userName);
+  //  console.log(userName)
+});
+
+let chatBox = document.getElementById("chatBox");
+chatBox.addEventListener("keyup", send);
+
+function send(e) {
+  if (e.key === "Enter") {
+    // console.log(chatBox.value)
+    socket.emit("new_message", {
+      userName,
+      message: chatBox.value,
+    });
+  }
+}
+
+socket.on("allMessagess", data => {
+  document.getElementById("messagess").innerHTML = data
+    .map((message) => `<br><b>${message.userName}</b>: ${message.message}`)
+    .join('');
+  });//mapeo para transformar el array en un array de string a ser unidos por el .join
--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 8080;
 const ready = () => console.log("server ready on port " + PORT);
-
+const chats= [];
 const http_server = server.listen(PORT, ready);
 const socket_server = new Server(http_server);
+socket_server.on("connection", socket =>{
+    // console.log(socket.client.id);
+    socket.on("auth", ()=>{
+        socket_server.emit("allMessagess",chats)
+    });
+    socket.on("new_message", data =>{ 
+        chats.push(data);
+        console.log(chats)
+        socket_server.emit("allMessagess", chats)
+    });
+    
+});
